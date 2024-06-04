f52727b442064a23b632b1347778731bd3302c0b
nanonymoussu <nanon2546@yahoo.com>
2024-06-05 06:38:29 +0700

    3d portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -43,7 +43,7 @@ const ProjectCard = ({
       </div>
 
       <div className={`w-full md:w-2/5 px-6 md:p-16 flex flex-col justify-center ${isEven ? "text-left md:text-left" : "text-left md:text-right"}`}>
-        <h3 className='text-white font-medium text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl lg:text-5xl leading-tight'>{name}</h3>
+        <h3 className='text-white font-medium text-md sm:text-lg md:text-xl xl:text-3xl 2xl:text-4xl lg:text-5xl leading-tight'>{name}</h3>
         <p className='mt-4 text-secondary text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg 2xl:text-xl'>{description}</p>
       </div>
     </motion.div>
